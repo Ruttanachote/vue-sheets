2aa23bd6a6e3168e488f418349a87d13dfe0a792
nuibonok555 <nui-jin@hotmail.com>
2023-03-27 11:08:18 +0700

    up

---
+++ src/components/Sheet.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3"
+        >
+          <h2>VueSheet List</h2>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <a
+              href="https://docs.google.com/spreadsheets/d/17K95YwoL3vM-PVPsR91_j7jjzgyMFIHL3Ju-Eb2raoQ/edit?usp=sharing"
+              class="btn btn-sm btn-outline-secondary"
+              target="_blank"
+            >
+              View Google Sheet
+            </a>
+          </div>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Activity</th>
+                <th>City</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <Row v-bind:key="row.id" v-for="row in rows" v-bind:row="row" />
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Row from "@/components/Row.vue";
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const creds = require("@/client_secret.json");
+export default {
+  name: "Sheet",
+  components: {
+    Row,
+  },
+  props: ["sheet"],
+  data() {
+    return {
+      rows: [],
+      loading: true,
+    };
+  },
+  methods: {
+    async accessSpreadSheet() {
+      const doc = new GoogleSpreadsheet("17K95YwoL3vM-PVPsR91_j7jjzgyMFIHL3Ju-Eb2raoQ");
+      await doc.useServiceAccountAuth(creds);
+      await doc.loadInfo();
+      const sheet = doc.sheetsByIndex[0];
+      const rows = await sheet.getRows();
+      this.rows = rows;
+    },
+  },
+  created() {
+    this.accessSpreadSheet();
+  },
+};
+</script>
+
+<style scoped></style>
